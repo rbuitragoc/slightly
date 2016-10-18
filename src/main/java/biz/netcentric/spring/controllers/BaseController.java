6668d3e7bbfa8b1bd77667a3bb665da2964fbdb1
Rick Buitrago <rbuitrago@velocitypartners.net>
2016-10-17 22:11:38 -0500

    making fm work using mav
    letting Spring's specific view resolver to handle this

--- src/main/java/biz/netcentric/spring/controllers/BaseController.java
+++ src/main/java/biz/netcentric/spring/controllers/BaseController.java
@@ -1,17 +1,34 @@
 package biz.netcentric.spring.controllers;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.support.RequestContextUtils;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import freemarker.core.ParseException;
+import freemarker.template.MalformedTemplateNameException;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateNotFoundException;
 
 @RestController
 public class BaseController {
   
   private static final Logger LOGGER = Logger.getLogger(BaseController.class);
+  
+  @Autowired
+  FreeMarkerConfigurer freeMarkerConfigurer;
 	
 	@RequestMapping("/base.sly")
 	public String base() {
@@ -21,9 +38,19 @@ public class BaseController {
 	}
 	
 	@RequestMapping("/templates.sly")
-	public String getTemplates(Model model, HttpServletRequest request) {
-		model.addAttribute("person.name", "Mr. Robertson");
-		return "index.sly";
+	public ModelAndView getTemplates(HttpServletRequest request, HttpServletResponse response) throws TemplateNotFoundException, MalformedTemplateNameException, ParseException, IOException, TemplateException {
+	  
+	  Map<String, Object> model = new HashMap<>();
+	  
+	  model.put("person", new HashMap<String, String>(){{put("name", "John");put("spouse", "Celia");}});
+	  model.put("child", "Michael");
+	  
+//	  Template t = freeMarkerConfigurer.getConfiguration().getTemplate("template1.html");
+//	  t.process(model, response.getWriter());
+	  
+	  ModelAndView mav = new ModelAndView("template1.html");
+	  mav.addAllObjects(model);
+		return mav;
 	}
 
 }
